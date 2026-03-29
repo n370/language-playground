dda4fda5a8c382d76f720edbc23d3d40fac1df2e
Dylson Valente Neto <ama@n370.info>
2026-03-29 17:08:17 -0300

    add go examples

--- go/frontend-masters-go-basics/0004/main.go
+++ go/frontend-masters-go-basics/0004/main.go
@@ -8,10 +8,13 @@ import (
 
 func main() {
 	cwd, _ := os.Getwd()
-	fileContents, err := fileutils.ReadTextFile(cwd + "/data/text.txt")
+	filePath := cwd + "/data/text.txt"
+	fileContents, err := fileutils.ReadTextFile(filePath)
 
 	if err == nil {
 		fmt.Println(fileContents)
+		newContent := fmt.Sprintf("Original: %vDouble original: %v%v", fileContents, fileContents, fileContents)
+		fileutils.WriteToFile(filePath+".output.txt", newContent)
 	} else {
 		fmt.Printf("ERROR PANIC!! %v", err)
 	}
