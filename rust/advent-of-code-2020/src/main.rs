c80cd1124db92d61c21c03c45e13be1ef0515ce8
Dylson Valente Neto <ama@n370.info>
2026-04-09 13:31:26 -0300

    add rust exercises

---
+++ rust/advent-of-code-2020/src/main.rs
@@ -0,0 +1,16 @@
+mod days;
+
+pub use days::{
+    day01,
+    day02,
+    day03,
+    day04,
+    day05,
+    day06,
+    day07,
+};
+
+fn main() {
+    // day07::exercise01::main();
+    day07::exercise02::main();
+}
