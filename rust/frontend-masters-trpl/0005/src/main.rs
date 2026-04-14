5f49f7d303e724810c3d4fa87fa9075a931c5cd3
Dylson Valente Neto <ama@n370.info>
2026-04-14 14:19:29 -0300

    add rust exercise

---
+++ rust/frontend-masters-trpl/0005/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
+
+    let sum_of_nums = sum(numbers.clone());
+    let product_of_nums = product(numbers.clone());
+    let average_of_nums = average(numbers);
+
+    println!("Sum of these numbers: {}", sum_of_nums);
+    println!("Product of these numbers: {}", product_of_nums);
+    println!("Average of these numbers: {}", average_of_nums);
+}
+
+fn sum(numbers: Vec<i64>) -> i64 {
+    let mut total = 0;
+
+    for num in numbers.iter() {
+        total += num;
+    }
+
+    total
+}
+
+fn product(numbers: Vec<i64>) -> i64 {
+    let mut total = 1;
+
+    for num in numbers.iter() {
+        total *= num;
+    }
+
+    total
+}
+
+fn average(numbers: Vec<i64>) -> i64 {
+    let length = numbers.len() as i64;
+
+    sum(numbers) / length
+}
