003b368a03396593fe28f6a55326b9d93a419f04
Dylson Valente Neto <ama@n370.info>
2026-04-15 23:23:11 -0300

    add rust exercise

---
+++ rust/frontend-masters-trpl/0007/src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    let numbers = vec![02, 04, 06, 08, 10, 12, 14, 16, 18, 20, 22, 24];
+    let sum_of_nums = sum(&numbers);
+    let product_of_nums = product(&numbers);
+    let average_of_nums = average(&numbers);
+
+    println!("Sum of these numbers: {}", sum_of_nums);
+    println!("Product of these numbers: {}", product_of_nums);
+    println!("Average of these numbers: {}", average_of_nums);
+
+    let other_numbers = vec![1, 2, 3, 4, 5, 6];
+    let (slice1, slice2) = first_three(&numbers, &other_numbers);
+
+    println!("the first three elements in `slice1` are:");
+
+    for num in slice1 {
+        println!("* {}", num);
+    }
+    
+    println!("The first three elements in `slice2` are:");
+
+    for num in slice2 {
+        println!("* {}", num);
+    }
+}
+
+fn sum(numbers: &[i64]) -> i64 {
+    let mut total = 0;
+
+    for num in numbers.iter() {
+        total += num;
+    }
+
+    total
+}
+
+fn product(numbers: &Vec<i64>) -> i64 {
+    let mut total = 0;
+
+    for num in numbers.iter() {
+        total *= num;
+    }
+
+    total
+}
+
+fn average(numbers: &[i64]) -> i64 {
+    let length = numbers.len() as i64;
+
+    sum(numbers) / length
+}
+
+fn first_three<'a,'b>(numbers1: &'a[i64], numbers2: &'b[i64]) -> (&'a[i64], &'b[i64]) {
+    (&numbers1[0..3], &numbers2[0..3])
+}
+
